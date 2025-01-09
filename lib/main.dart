a01ba5a329673993774121a718d139ff112ba2fe
Malak Mohammed <105985670+Malak173@users.noreply.github.com>
2025-01-09 20:40:28 +0200

    uploadtoGitHub

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,28 @@ class PointsCounter extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 204, 106, 7),
+          backgroundColor: const Color.fromARGB(255, 207, 124, 41),
           title:const Text('Pints Counter',style: TextStyle(color: Colors.white),) ,
         ),
         body: Column(
+          children: [
+            Text('Team A',style: TextStyle(
+              color: Colors.black ,
+              fontSize: 45,
+            ),),
+            Text('0',style: TextStyle(
+              color: Colors.black,
+              fontSize: 120,
+            ),),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                 foregroundColor: Colors.black,
+                 backgroundColor: const Color.fromARGB(255, 207, 124, 41),
+              ),
+              onPressed: (){}, 
+              child: Text('Add 1 Point'),),
           
+          ],
         ),
       ),
     );
